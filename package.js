42cb2dbc544f2b9175c4720ff817e6bfce1e2dd0
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:32:04 -0500

    Updated component to version 1.9.1

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-api',
+  summary : 'Semantic UI - Api (official): Single component release of api',
+  version : '1.9.1',
+  git     : 'git://github.com/Semantic-Org/UI-Api.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
