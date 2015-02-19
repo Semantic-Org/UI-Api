04ccb2510ea187d91fb9192f48811e1d7c6f88ef
Pusher Robot <jack@semantic-ui.com>
2015-02-19 18:09:20 -0500

    Updated component to version 1.9.2

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ var
 Package.describe({
   name    : 'semantic:ui-api',
   summary : 'Semantic UI - Api (official): Single component release of api',
-  version : '1.9.1',
+  version : '1.9.2',
   git     : 'git://github.com/Semantic-Org/UI-Api.git',
 });
 
