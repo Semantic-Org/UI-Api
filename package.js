3148b38ee2dc319cb80edbf9db973c93128d35ed
Jack Lukic <jack@semantic-ui.com>
2017-02-07 10:52:44 -0500

    Updated component to version 2.2.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-api',
   summary : 'Semantic UI - Api: Single component release',
-  version : '2.2.6',
+  version : '2.2.7',
   git     : 'git://github.com/Semantic-Org/UI-Api.git',
 });
 
